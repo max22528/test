6c3c417a9e51fb616ce4a2fa8e32ab662fb8db1f
max22528 <newgod1992@gmail.com>
2015-01-25 13:20:51 +0800

    weight 0 -> 10

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,7 @@ angular.module('app',['changeNumberAnimate'])
 .controller('testA', ['$scope','changeNumberAnimate', function ($scope,numberAnimate){
   $scope.speed = 100000;
   $scope.weight ={}; 
-  $scope.weight.input = 0; 
+  $scope.weight.input = 10; 
   $scope.weight.newNum = 0;  
   $scope.numberChange = function(){ 
     numberAnimate.changeNumber.apply($scope.weight, [$scope.speed]);
